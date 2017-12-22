2930e1f0563b9af8faeb177ea33326c2acc42304
marcossuarez <info.marcossuarez@gmail.com>
2017-12-22 13:48:28 +0100

    .
    .

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -20,34 +20,6 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        document.addEventListener("deviceready", onDeviceReady, false);
-        function onDeviceReady() {
-            console.log('Application Ready');
-            if (checkConnection()) {
-              // var ref = window.open(encodeURI('https://example.com'), '_self', 'location=no');
-            } else {
-              var ref = window.open('offline.html', '_self', 'location=no');
-            }
-        }
-        function checkConnection() {
-            var networkState = navigator.connection.type;
-
-            var states = {};
-            states[Connection.UNKNOWN]  = 'Unknown connection';
-            states[Connection.ETHERNET] = 'Ethernet connection';
-            states[Connection.WIFI]     = 'WiFi connection';
-            states[Connection.CELL_2G]  = 'Cell 2G connection';
-            states[Connection.CELL_3G]  = 'Cell 3G connection';
-            states[Connection.CELL_4G]  = 'Cell 4G connection';
-            states[Connection.CELL]     = 'Cell generic connection';
-            states[Connection.NONE]     = 'No network connection';
-
-            if (states[networkState] == 'No network connection') {
-              return false;
-            } else {
-              return true;
-            }
-        }
     },
     // Bind Event Listeners
     //
