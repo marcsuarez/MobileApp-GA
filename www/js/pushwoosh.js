a1fbde8a33867e117cc709ec9f1afb891e2478fd
marcossuarez <info.marcossuarez@gmail.com>
2017-12-28 14:16:05 +0100

    Pushwoosh
    Pushwoosh

---
+++ www/js/pushwoosh.js
@@ -0,0 +1,32 @@
+function initPushwoosh() {
+    var pushwoosh = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+  
+    // Should be called before pushwoosh.onDeviceReady
+    document.addEventListener('push-notification', function(event) {
+      var notification = event.notification;
+      // handle push open here
+    });
+    
+    // Initialize Pushwoosh. This will trigger all pending push notifications on start.
+    pushwoosh.onDeviceReady({
+      appid: "6EE6E-9BF27",
+      projectid: "658431314297",
+      serviceName: "MPNS_SERVICE_NAME"
+    });
+}
+
+pushwoosh.registerDevice(
+    function(status) {
+      var pushToken = status.pushToken;
+        // handle successful registration here
+    },
+    function(status) {
+      // handle registration error here
+    }
+);
+
+// should be called before pushwoosh.onDeviceReady
+document.addEventListener('push-notification', function(event) {
+    var notification = event.notification;
+    // handle push open here
+  });
