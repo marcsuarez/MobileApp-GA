afbcfe5588777b2aff84d3aa43f99f7c5184d69d
marcossuarez <info.marcossuarez@gmail.com>
2018-01-02 10:14:02 +0100

    .
    .

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,40 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+function initPushwoosh() {
+    var pushwoosh = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+  
+    // Should be called before pushwoosh.onDeviceReady
+    document.addEventListener('push-notification', function(event) {
+      var notification = event.notification;
+      // handle push open here
+    });
+    
+    // Initialize Pushwoosh. This will trigger all pending push notifications on start.
+    pushwoosh.onDeviceReady({
+      appid: "6EE6E-9BF27",
+      projectid: "658431314297",
+      serviceName: ""
+    });
+}
+
+pushwoosh.registerDevice(
+    function(status) {
+      var pushToken = status.pushToken;
+        // handle successful registration here
+    },
+    function(status) {
+      // handle registration error here
+    }
+);
+
+// should be called before pushwoosh.onDeviceReady
+document.addEventListener('push-notification', function(event) {
+    var notification = event.notification;
+    // handle push open here
+});
+
 var app = {
     // Application Constructor
     initialize: function() {
